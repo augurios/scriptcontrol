da79204eee0c6530e3860022ff5355be5c6f2304
Augusto Valerio <auguvalerio@gmail.com>
2018-01-12 11:08:02 -0600

    initial

---
+++ scriptcontrol.js
@@ -0,0 +1,26 @@
+[
+  {
+    // App1
+    "uid": "app1",
+    "append": true,
+    "watch": true,
+    "script": "app.js",
+    "sourceDir": "/home/bitnami/apps/overtableapi"
+  },
+  {
+    // App2
+    "uid": "app2",
+    "append": true,
+    "watch": true,
+    "script": "app.js",
+    "sourceDir": "/home/bitnami/apps/overtableadmin"
+  }/*,
+  {
+    // forever,Control
+    "uid": "app2",
+    "append": true,
+    "watch": true,
+    "script": "repeater.js",
+    "sourceDir": "/home/bitnami/apps/scriptcontrol"
+  }*/
+]
